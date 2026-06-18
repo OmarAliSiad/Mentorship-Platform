57d3bf667f3a41c6b12ad219b4b54554ce861d23
mohamedahmed-dev <mohamedahmed010320@gmail.com>
2026-06-18 09:12:00 +0300

    feat(mentor): add delete mentor availability route

--- Backend/src/routes/mentorRoutes.js
+++ Backend/src/routes/mentorRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getMentorProfile, updateMentorProfile, getMentorAvailability, createMentorAvailability, updateMentorAvailability } from '../controllers/mentorController.js';
+import { getMentorProfile, updateMentorProfile, getMentorAvailability, createMentorAvailability, updateMentorAvailability, deleteMentorAvailability } from '../controllers/mentorController.js';
 import { protect, authorizeRoles } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
@@ -9,5 +9,6 @@ router.put('/profile', protect, authorizeRoles('Mentor'), updateMentorProfile);
 router.get('/availability', protect, authorizeRoles('Mentor'), getMentorAvailability);
 router.post('/availability', protect, authorizeRoles('Mentor'), createMentorAvailability);
 router.put('/availability/:id', protect, authorizeRoles('Mentor'), updateMentorAvailability);
+router.delete('/availability/:id', protect, authorizeRoles('Mentor'), deleteMentorAvailability);
 
 export default router;
